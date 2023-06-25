bac96007d68e4a4edfce19964272c0ef5af9f2d4
Yilun Guo <yg2433@columbia.edu>
2023-06-25 09:13:50 -0400

    Update hw1.py
    prob 1

--- wk1_recursion/hw1.py
+++ wk1_recursion/hw1.py
@@ -7,4 +7,13 @@ class ListNode:
          self.next = next
 
 def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    pass
+        pre_head = ListNode()
+        cur = pre_head
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next, list1 = list1, list1.next
+            else:
+                cur.next, list2 = list2, list2.next
+            cur = cur.next
+        cur.next = list1 or list2
+        return pre_head.next
