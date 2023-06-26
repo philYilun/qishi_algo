64ec5fcc09458e74dd89adc345b98f193421e7e7
Yilun Guo <yg2433@columbia.edu>
2023-06-26 09:17:01 -0400

    Update hw1.py
    update prob 5

--- wk1_recursion/hw1.py
+++ wk1_recursion/hw1.py
@@ -86,25 +86,41 @@ class Solution:
 class Solution:
     def validTicTacToe(self, board: List[str]) -> bool:
  
-# The two criteria for a valid board are:
+# The validate state has 2 criteria
+# the state is at the middle of the game
+# the state is valid
 #   1) num of Xs - num of Os is 0 or 1
-#   2) X is not a winner if the # of moves is even, and
+#   2) the state is not a winning state:
+#      X is not a winner if the # of moves is even, and
 #      O is not a winner if the # of moves is odd.
-
-        d = {'X': 1, 'O': -1, ' ': 0}               # transform the 1x3 str array to a 1x9 int array
-        s = [d[ch] for ch in ''.join(board)]        # Ex: ["XOX"," X ","   "] --> [1,-1,1,0,1,0,0,0,0]
+        # map the character to number
+        m = {'X': 1, 
+             'O': -1, 
+             ' ': 0}               
+        
+        # transform the 1x3 str array to a 1x9 int array
+        s = [m[char] for char in ''.join(board)]        
+        # Ex: ["XOX"," X ","   "] --> [1,-1,1,0,1,0,0,0,0]
+        
         sm = sum(s)
 
-        if sm>>1: return False       # <-- criterion 1
+        # identify invalid state
+        if sm>>1: return False        
         
-        n = -3 if sm == 1 else 3     # <-- criterion 2.
+        if sm == 1: 
+            n = -3 
+        else :
+            n = 3     
+        
+        # identify not winning state
         if n in {s[0]+s[1]+s[2], s[3]+s[4]+s[5], s[6]+s[7]+s[8], 
                  s[0]+s[3]+s[6], s[1]+s[4]+s[7], s[2]+s[5]+s[8],         
                  # the elements of the set are 
-                 s[0]+s[4]+s[8], s[2]+s[4]+s[6]}: return False        
-
+                 s[0]+s[4]+s[8], s[2]+s[4]+s[6]}: return False           
                  # the rows, cols, and diags
-        return True            
+        
+        return True                                                 
+        # <-- both criteria are true
 
 
 #q6
