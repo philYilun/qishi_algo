a9f908880a38fd156b42a5cb648fb6cabd884480
Yilun Guo <yg2433@columbia.edu>
2023-06-25 20:06:08 -0400

    Update hw1.py
    problem 3 result

--- wk1_recursion/hw1.py
+++ wk1_recursion/hw1.py
@@ -32,3 +32,21 @@ class Solution:
             else:
                 return False
         return helper(n)
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        head_tmp = ListNode()
+        if not head:
+            return head
+        head.next = self.removeElements(head.next, val)
+        
+        if head.val == val:
+            head = head.next
+        
+        return head
