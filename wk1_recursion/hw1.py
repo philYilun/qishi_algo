0c958fe469aad7cc09c60ab4df99e2a66c238822
Yilun Guo <yg2433@columbia.edu>
2023-06-25 21:05:59 -0400

    Update hw1.py
    q5

--- wk1_recursion/hw1.py
+++ wk1_recursion/hw1.py
@@ -52,20 +52,56 @@ class Solution:
         return head
 
 
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#q4
 class Solution:
-    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        head_tmp = ListNode()
-        if not head:
-            return head
-        head.next = self.removeElements(head.next, val)
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total_value = sum(nums)
+        if total_value % k != 0:
+            return False
         
-        if head.val == val:
-            head = head.next
+        sub_sum = total_value // k
+        subset = [0] * k
+        nums.sort(reverse = True)
+
+        def recurse(i):
+            if i == len(nums):
+                return True
+            
+            for j in range(k):
+                if subset[j] + nums[i] <= sub_sum:
+                    subset[j] += nums[i]
+
+                    if recurse(i + 1):
+                        return True
+                    
+                    subset[j] -= nums[i]
+
+                    if subset[j] == 0:
+                        break
+            return False
+        return recurse(0)
+
+
+#q5
+class Solution:
+    def validTicTacToe(self, board: List[str]) -> bool:
+ 
+# The two criteria for a valid board are:
+#   1) num of Xs - num of Os is 0 or 1
+#   2) X is not a winner if the # of moves is even, and
+#      O is not a winner if the # of moves is odd.
+
+        d = {'X': 1, 'O': -1, ' ': 0}               # transform the 1x3 str array to a 1x9 int array
+        s = [d[ch] for ch in ''.join(board)]        # Ex: ["XOX"," X ","   "] --> [1,-1,1,0,1,0,0,0,0]
+        sm = sum(s)
+
+        if sm>>1: return False       # <-- criterion 1
         
-        return head
+        n = -3 if sm == 1 else 3     # <-- criterion 2.
+        if n in {s[0]+s[1]+s[2], s[3]+s[4]+s[5], s[6]+s[7]+s[8], 
+                 s[0]+s[3]+s[6], s[1]+s[4]+s[7], s[2]+s[5]+s[8],         
+                 # the elements of the set are 
+                 s[0]+s[4]+s[8], s[2]+s[4]+s[6]}: return False           
+                 # the rows, cols, and diags
+        
+        return True            
